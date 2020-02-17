57c14a63ccb7dd52e3acb95c7dc8e6ba032ea9fc
Wisarut Khumngoen <wk.stta@gmail.com>
2020-02-17 10:02:27 +0700

    Add proxy url to Dockerfile

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #Start redis server on 22122
-redis-server --port 22122 &
+#redis-server --port 22122 &
+redis-server - <<EOF &
+port 22122
+save 900 1
+save 300 10
+save 60 10000
+dir  /var/lib/redis
+EOF
 
 src/dynomite --conf-file=conf/redis_single.yml -v5
